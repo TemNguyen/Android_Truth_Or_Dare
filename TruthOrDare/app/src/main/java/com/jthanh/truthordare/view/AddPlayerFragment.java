494fedb18673342a4f30232fda80e4f82bde47e3
Nguyễn Duy Thịnh <ndthinhdut@outlook.com>
2022-06-09 16:49:33 +0700

    Add room database, Add feature 'Add and remove players', Add feature 'Store last player in local database'

--- TruthOrDare/app/src/main/java/com/jthanh/truthordare/view/AddPlayerFragment.java
+++ TruthOrDare/app/src/main/java/com/jthanh/truthordare/view/AddPlayerFragment.java
@@ -5,10 +5,8 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
 import androidx.navigation.Navigation;
 
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -16,6 +14,8 @@ import android.view.ViewGroup;
 import com.jthanh.truthordare.R;
 import com.jthanh.truthordare.databinding.CustomActionBarBinding;
 import com.jthanh.truthordare.databinding.FragmentAddPlayerBinding;
+import com.jthanh.truthordare.model.AppDatabase;
+import com.jthanh.truthordare.model.DatabaseDao;
 import com.jthanh.truthordare.model.Player;
 import com.jthanh.truthordare.viewmodel.AddPlayerAdapter;
 
@@ -26,6 +26,8 @@ public class AddPlayerFragment extends Fragment {
     private CustomActionBarBinding actionBarBinding;
     private ArrayList<Player> players;
     private AddPlayerAdapter adapter;
+    private AppDatabase appDatabase;
+    private DatabaseDao databaseDao;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -34,7 +36,6 @@ public class AddPlayerFragment extends Fragment {
 
         }
     }
-
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         actionBarBinding.tvTitle.setText("Thêm người chơi");
@@ -45,16 +46,17 @@ public class AddPlayerFragment extends Fragment {
             }
         });
 
-        players = new ArrayList<>();
-        players.add(new Player(0, "Thanh"));
-        players.add(new Player(1, "Loc"));
+        appDatabase = AppDatabase.getInstance(this.getContext());
+        databaseDao = appDatabase.databaseDao();
+        players = new ArrayList<>(databaseDao.getAllPlayer());
+
         adapter = new AddPlayerAdapter(players);
         binding.rvPlayers.setAdapter(adapter);
 
         binding.btnAddPlayer.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                players.add(new Player(3, ""));
+                players.add(new Player(""));
                 adapter.notifyDataSetChanged();
             }
         });
@@ -67,6 +69,8 @@ public class AddPlayerFragment extends Fragment {
                         return;
                     }
                 }
+                databaseDao.deleteAllPlayer();
+                databaseDao.insertAllPlayer(players);
                 Bundle bundle = new Bundle();
                 bundle.putSerializable("listPlayer", players);
                 Navigation.findNavController(view).navigate(R.id.chooseQuestion, bundle);
