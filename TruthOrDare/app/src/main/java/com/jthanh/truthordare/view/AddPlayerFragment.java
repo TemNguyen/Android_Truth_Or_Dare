b9a49f3322aec3c028a9a82d3221977b4ace3206
VoVanThanh <thanhvo618@gmail.com>
2022-06-09 10:54:12 +0700

    done UI add player

--- TruthOrDare/app/src/main/java/com/jthanh/truthordare/view/AddPlayerFragment.java
+++ TruthOrDare/app/src/main/java/com/jthanh/truthordare/view/AddPlayerFragment.java
@@ -2,15 +2,27 @@ package com.jthanh.truthordare.view;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.jthanh.truthordare.R;
+import com.jthanh.truthordare.databinding.FragmentAddPlayerBinding;
+import com.jthanh.truthordare.model.Player;
+import com.jthanh.truthordare.viewmodel.AddPlayerAdapter;
+
+import java.util.ArrayList;
 
 public class AddPlayerFragment extends Fragment {
+    private FragmentAddPlayerBinding binding;
+    private ArrayList<Player> players;
+    private AddPlayerAdapter adapter;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -21,8 +33,47 @@ public class AddPlayerFragment extends Fragment {
     }
 
     @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        players = new ArrayList<>();
+        players.add(new Player(0, "Thanh"));
+        players.add(new Player(1, "Loc"));
+        adapter = new AddPlayerAdapter(players);
+        binding.rvPlayers.setAdapter(adapter);
+
+        binding.btnAddPlayer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                players.add(new Player(3, ""));
+                adapter.notifyDataSetChanged();
+            }
+        });
+
+        binding.btnPlay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                for (Player player : players) {
+                    if (player.getName().isEmpty()) {
+                        return;
+                    }
+                }
+                Bundle bundle = new Bundle();
+                bundle.putSerializable("listPlayer", players);
+                Navigation.findNavController(view).navigate(R.id.gameFragment, bundle);
+            }
+        });
+    }
+
+    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_add_player, container, false);
+        binding = FragmentAddPlayerBinding.inflate(inflater, container, false);
+        View view = binding.getRoot();
+        return view;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
     }
 }
---
+++ TruthOrDare/app/src/main/java/com/jthanh/truthordare/viewmodel/AddPlayerAdapter.java
@@ -0,0 +1,65 @@
+package com.jthanh.truthordare.viewmodel;
+
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.jthanh.truthordare.R;
+import com.jthanh.truthordare.databinding.AddPlayerItemBinding;
+import com.jthanh.truthordare.model.Player;
+
+import java.util.ArrayList;
+
+public class AddPlayerAdapter extends RecyclerView.Adapter<AddPlayerAdapter.ViewHolder> {
+    private ArrayList<Player> players;
+
+    public AddPlayerAdapter(ArrayList<Player> data) {
+        players = data;
+    }
+
+    @NonNull
+    @Override
+    public AddPlayerAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return new ViewHolder(AddPlayerItemBinding.inflate(LayoutInflater.from(parent.getContext()), parent, false));
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull AddPlayerAdapter.ViewHolder holder, int position) {
+        holder.binding.etPlayer.setText(players.get(position).getName());
+        holder.binding.etPlayer.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                players.get(holder.getAdapterPosition()).setName(editable.toString());
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return players == null ? 0 : players.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        private final AddPlayerItemBinding binding;
+
+        public ViewHolder(final AddPlayerItemBinding itemBinding) {
+            super(itemBinding.getRoot());
+            this.binding = itemBinding;
+        }
+    }
+}
