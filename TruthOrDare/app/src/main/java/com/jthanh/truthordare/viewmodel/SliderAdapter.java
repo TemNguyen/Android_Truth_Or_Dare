48c8d1cfd178d70f6c935feb0cdb9754e7f3436a
Nguyễn Duy Thịnh <ndthinhdut@outlook.com>
2022-06-19 15:52:31 +0700

    Add feature random question with truth or dare, Add message if no truth or dare question

--- TruthOrDare/app/src/main/java/com/jthanh/truthordare/viewmodel/SliderAdapter.java
+++ TruthOrDare/app/src/main/java/com/jthanh/truthordare/viewmodel/SliderAdapter.java
@@ -1,6 +1,8 @@
 package com.jthanh.truthordare.viewmodel;
 
+import android.app.Activity;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,6 +14,7 @@ import androidx.viewpager2.widget.ViewPager2;
 
 import com.jthanh.truthordare.R;
 import com.jthanh.truthordare.databinding.SliderItemBinding;
+import com.jthanh.truthordare.dialogs.NotificationDialog;
 import com.jthanh.truthordare.model.entities.PackageSelect;
 import com.jthanh.truthordare.model.entities.Player;
 import com.jthanh.truthordare.model.entities.Question;
@@ -24,10 +27,16 @@ import java.util.ArrayList;
 import java.util.Random;
 
 public class SliderAdapter extends RecyclerView.Adapter<SliderAdapter.SliderViewHolder> {
+    private Activity activity;
+
     private ArrayList<Player> sliderItems;
     private ViewPager2 viewPager2;
     private ArrayList<PackageSelect> packageSelected;
+
     private ArrayList<Question> listQuestion;
+    private ArrayList<Question> listTruthQuestion;
+    private ArrayList<Question> listDareQuestion;
+    private int listQuestionSize;
 
     private AppDatabase appDatabase;
     private QuestionPackageDao questionPackageDao;
@@ -35,7 +44,11 @@ public class SliderAdapter extends RecyclerView.Adapter<SliderAdapter.SliderView
 
     private Random random;
 
-    public SliderAdapter(ArrayList<Player> sliderItems, ViewPager2 viewPager2, ArrayList<PackageSelect> packageSelected) {
+    private NotificationDialog notificationDialog;
+
+    public SliderAdapter(ArrayList<Player> sliderItems, ViewPager2 viewPager2,
+                         ArrayList<PackageSelect> packageSelected, Activity activity) {
+        this.activity = activity;
         this.sliderItems = sliderItems;
         this.viewPager2 = viewPager2;
         this.packageSelected = packageSelected;
@@ -53,10 +66,15 @@ public class SliderAdapter extends RecyclerView.Adapter<SliderAdapter.SliderView
     public void onBindViewHolder(@NonNull SliderViewHolder holder, int position) {
 
         listQuestion = new ArrayList<>();
+        listTruthQuestion = new ArrayList<>();
+        listDareQuestion = new ArrayList<>();
+
         appDatabase = AppDatabase.getInstance(holder.itemView.getContext());
         questionDao = appDatabase.questionDao();
         questionPackageDao = appDatabase.questionPackageDao();
         random = new Random();
+
+        notificationDialog = new NotificationDialog(activity);
 
         for (PackageSelect selected:
                 packageSelected) {
@@ -64,7 +82,14 @@ public class SliderAdapter extends RecyclerView.Adapter<SliderAdapter.SliderView
             listQuestion.addAll(questionDao.getQuestionByPackageId(questionPackage.getId()));
         }
 
-
+        for (Question question:
+             listQuestion) {
+            if (question.getRule().equals("truth")) {
+                listTruthQuestion.add(question);
+            } else {
+                listDareQuestion.add(question);
+            }
+        }
 
         holder.binding.tvPlayer.setText(sliderItems.get(position).getName());
         if (position == sliderItems.size() - 2) {
@@ -73,25 +98,46 @@ public class SliderAdapter extends RecyclerView.Adapter<SliderAdapter.SliderView
         holder.binding.llTruth.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Bundle bundle = new Bundle();
-                Question rdQuestion = listQuestion.get(random.nextInt(listQuestion.size()));
-                bundle.putSerializable("question", rdQuestion);
-                bundle.putSerializable("player", sliderItems.get(holder.getAdapterPosition()));
-                bundle.putSerializable("type", "Thật");
-                Navigation.findNavController(holder.itemView).navigate(R.id.gameDetailFragment, bundle);
+                listQuestionSize = listTruthQuestion.size();
+                if (listQuestionSize > 0) {
+                    Question rdQuestion = listTruthQuestion.get(random.nextInt(listQuestionSize));
+                    Bundle bundle = new Bundle();
+                    bundle.putSerializable("question", rdQuestion);
+                    bundle.putSerializable("player", sliderItems.get(holder.getAdapterPosition()));
+                    bundle.putSerializable("type", "Thật");
+                    Navigation.findNavController(holder.itemView).navigate(R.id.gameDetailFragment, bundle);
+                } else {
+                    showMessage("Không có câu hỏi thật nào :(", false);
+                }
             }
         });
         holder.binding.llDare.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Bundle bundle = new Bundle();
-                Question rdQuestion = listQuestion.get(random.nextInt(listQuestion.size()));
-                bundle.putSerializable("question", rdQuestion);
-                bundle.putSerializable("player", sliderItems.get(holder.getAdapterPosition()));
-                bundle.putSerializable("type", "Thách");
-                Navigation.findNavController(holder.itemView).navigate(R.id.gameDetailFragment, bundle);
+                listQuestionSize = listDareQuestion.size();
+                if (listQuestionSize > 0) {
+                    Bundle bundle = new Bundle();
+                    Question rdQuestion = listDareQuestion.get(random.nextInt(listDareQuestion.size()));
+                    bundle.putSerializable("question", rdQuestion);
+                    bundle.putSerializable("player", sliderItems.get(holder.getAdapterPosition()));
+                    bundle.putSerializable("type", "Thách");
+                    Navigation.findNavController(holder.itemView).navigate(R.id.gameDetailFragment, bundle);
+                } else {
+                    showMessage("Không có câu hỏi thách nào :(", false);
+                }
             }
         });
+    }
+
+    private void showMessage(String msg, boolean state) {
+        notificationDialog.startDialog(msg, state);
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                notificationDialog.dismissDialog();
+            }
+        }, 1000);
     }
 
     @Override
